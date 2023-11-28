35322a18c8a0410fb250ed4a30928e53cb8c7023
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2023-11-29 01:09:52 +0700

    Update form state hook

--- src/hooks/useFormState.ts
+++ src/hooks/useFormState.ts
@@ -9,38 +9,25 @@ export default function useFormState<T extends Object>(
   const [data, setData] = useState(initialData);
   const [errors, setErrors] = useState<Record<keyof T, string>>(() => {
     return Object.keys(validators).reduce((acc, key) => {
-      acc[key as keyof T] = "";
+      acc[key as keyof T] =
+        validators[key as keyof T]!(initialData[key as keyof T]) ?? "";
       return acc;
     }, {} as Record<keyof T, string>);
-  });
-  const [touched, setTouched] = useState<Record<keyof T, boolean>>(() => {
-    return Object.keys(validators).reduce((acc, key) => {
-      acc[key as keyof T] = false;
-      return acc;
-    }, {} as Record<keyof T, boolean>);
   });
 
   const updateData = <K extends keyof T>(key: K, value: T[K]) => {
     if (validators[key]) {
       setErrors((prev) => ({ ...prev, [key]: validators[key]!(value) }));
     }
-    setTouched((prev) => ({ ...prev, [key]: true }));
+
     setData((prev) => ({ ...prev, [key]: value }));
   };
 
   const resetData = () => {
     setData(initialData);
-    setTouched((prev) => {
-      return Object.keys(prev).reduce((acc, key) => {
-        acc[key as keyof T] = false;
-        return acc;
-      }, {} as Record<keyof T, boolean>);
-    });
   };
 
-  const isValid =
-    Object.values(errors).every((error) => !error) &&
-    Object.values(touched).every((touched) => touched);
+  const isValid = Object.values(errors).every((error) => error === "");
 
   return {
     data,
