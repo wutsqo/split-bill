a8655cdb6f53a07a9c4d50392f2c74799b8924f3
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2023-11-29 13:46:19 +0700

    Add PersonLabel component

---
+++ src/app/app/person.tsx
@@ -0,0 +1,38 @@
+import { FC } from "react";
+import { createAvatar } from "@dicebear/core";
+import { funEmoji } from "@dicebear/collection";
+import { mergeClassname } from "@/utils/common";
+
+interface PersonLabelProps {
+  name?: string;
+  size?: "sm" | "md" | "lg";
+  suffix?: string;
+  prefix?: string;
+}
+
+const sizeMap = {
+  sm: "w-5 h-5",
+  md: "w-7 h-7",
+  lg: "w-10 h-10",
+};
+
+export const PersonLabel: FC<PersonLabelProps> = ({
+  name = "Deleted Person",
+  size = "md",
+  suffix = "",
+  prefix = "",
+}) => {
+  return (
+    <div className="text-sm flex items-center gap-2">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src={createAvatar(funEmoji, {
+          seed: name,
+        }).toDataUriSync()}
+        alt={name}
+        className={mergeClassname("mask mask-squircle", sizeMap[size])}
+      />
+      {prefix} {name} {suffix}
+    </div>
+  );
+};
