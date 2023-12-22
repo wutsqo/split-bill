d12aff1ac61db9514efccc62c4aa20694d3fcd16
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2023-12-22 20:10:10 +0700

    Update SITE_URL in config.ts

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
 export const appName = "SplitBill";
-export const SITE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL
+  ? `https://${process.env.NEXT_PUBLIC_SITE_URL}`
   : "http://localhost:3000";
