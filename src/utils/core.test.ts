edec7480abe1ceb779746ae417dc10b2cf4c9e29
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2023-12-24 12:16:42 +0700

    Add new functions and tests for core functionality

--- src/utils/core.test.ts
+++ src/utils/core.test.ts
@@ -1,5 +1,9 @@
-import { SplitType, Transaction } from "@/app/app/type";
-import { calculatePortion, generateDebtFromTransaction } from "./core";
+import { Debt, Person, SplitType, Transaction } from "@/app/app/type";
+import {
+  calculatePortion,
+  generateDebtFromTransaction,
+  getBalanceOfAPerson,
+} from "./core";
 
 const trxBuilder = ({
   amount,
@@ -194,5 +198,264 @@ describe("generateDebtFromTransaction", () => {
     ],
   ])("%s", (_, expected, ...args) => {
     expect(generateDebtFromTransaction(...args)).toEqual(expected);
+  });
+});
+
+const personBuilder = ({ id, name }: { id: string; name: string }): Person => ({
+  id,
+  name,
+  balance: 0,
+  paysTo: {},
+});
+
+const PERSON_1 = personBuilder({ id: "1", name: "name_1" });
+const PERSON_2 = personBuilder({ id: "2", name: "name_2" });
+
+describe("personBuilder", () => {
+  it("return id correctly", () => {
+    expect(PERSON_1.id).toBe("1");
+    expect(PERSON_2.id).toBe("2");
+  });
+
+  it("return name correctly", () => {
+    expect(PERSON_1.name).toBe("name_1");
+    expect(PERSON_2.name).toBe("name_2");
+  });
+
+  it("return balance correctly", () => {
+    expect(PERSON_1.balance).toBe(0);
+    expect(PERSON_2.balance).toBe(0);
+  });
+
+  it("return paysTo correctly", () => {
+    expect(PERSON_1.paysTo).toEqual({});
+    expect(PERSON_2.paysTo).toEqual({});
+  });
+});
+
+const debtsBuilder = ({
+  lenderIds,
+  borrowerIds,
+  amounts,
+  transactionIds,
+}: {
+  lenderIds: string[];
+  borrowerIds: string[];
+  amounts: number[];
+  transactionIds: string[];
+}): Debt[] => {
+  return lenderIds.reduce((acc, lenderId, index) => {
+    const borrowerId = borrowerIds[index];
+    const amount = amounts[index];
+    const transactionId = transactionIds[index];
+    return [
+      ...acc,
+      {
+        lenderId,
+        borrowerId,
+        amount,
+        transactionId,
+      },
+    ];
+  }, [] as Debt[]);
+};
+
+describe("debtsBuilder", () => {
+  it("return correctly", () => {
+    expect(
+      debtsBuilder({
+        lenderIds: ["1", "2"],
+        borrowerIds: ["2", "1"],
+        amounts: [49, 49],
+        transactionIds: ["1", "2"],
+      })
+    ).toEqual([
+      {
+        lenderId: "1",
+        borrowerId: "2",
+        amount: 49,
+        transactionId: "1",
+      },
+      {
+        lenderId: "2",
+        borrowerId: "1",
+        amount: 49,
+        transactionId: "2",
+      },
+    ]);
+  });
+});
+
+describe("getBalanceOfAPerson", () => {
+  it.each([
+    [
+      "return correctly for person with no debt",
+      {
+        ...PERSON_1,
+        balance: 0,
+        paysTo: {} as Record<string, number>,
+      },
+      PERSON_1,
+      [] as Debt[],
+    ],
+    [
+      "return correctly for lender from single transaction",
+      {
+        ...PERSON_1,
+        balance: 49,
+        paysTo: {
+          "2": -49,
+        },
+      },
+      PERSON_1,
+      debtsBuilder({
+        lenderIds: ["1"],
+        borrowerIds: ["2"],
+        amounts: [49],
+        transactionIds: ["1"],
+      }),
+    ],
+    [
+      "return correctly for borrower from single transaction",
+      {
+        ...PERSON_2,
+        balance: -49,
+        paysTo: {
+          "1": 49,
+        },
+      },
+      PERSON_2,
+      debtsBuilder({
+        lenderIds: ["1"],
+        borrowerIds: ["2"],
+        amounts: [49],
+        transactionIds: ["1"],
+      }),
+    ],
+    [
+      "return correctly for lender from two transactions same borrower",
+      {
+        ...PERSON_1,
+        balance: 99,
+        paysTo: {
+          "2": -99,
+        },
+      },
+      PERSON_1,
+      debtsBuilder({
+        lenderIds: ["1", "1"],
+        borrowerIds: ["2", "2"],
+        amounts: [49, 50],
+        transactionIds: ["1", "2"],
+      }),
+    ],
+    [
+      "return correctly for lender from two transactions different borrower",
+      {
+        ...PERSON_1,
+        balance: 79,
+        paysTo: {
+          "2": -49,
+          "3": -30,
+        },
+      },
+      PERSON_1,
+      debtsBuilder({
+        lenderIds: ["1", "1"],
+        borrowerIds: ["2", "3"],
+        amounts: [49, 30],
+        transactionIds: ["1", "2"],
+      }),
+    ],
+    [
+      "return correctly for borrower from two transactions same lender",
+      {
+        ...PERSON_2,
+        balance: -99,
+        paysTo: {
+          "1": 99,
+        },
+      },
+      PERSON_2,
+      debtsBuilder({
+        lenderIds: ["1", "1"],
+        borrowerIds: ["2", "2"],
+        amounts: [49, 50],
+        transactionIds: ["1", "2"],
+      }),
+    ],
+    [
+      "return correctly for borrower from two transactions different lender",
+      {
+        ...PERSON_2,
+        balance: -79,
+        paysTo: {
+          "1": 49,
+          "3": 30,
+        },
+      },
+      PERSON_2,
+      debtsBuilder({
+        lenderIds: ["1", "3"],
+        borrowerIds: ["2", "2"],
+        amounts: [49, 30],
+        transactionIds: ["1", "2"],
+      }),
+    ],
+    [
+      "return correctly for lender and borrower from two transactions same lender and borrower",
+      {
+        ...PERSON_1,
+        balance: 0,
+        paysTo: {
+          "2": 0,
+        },
+      },
+      PERSON_1,
+      debtsBuilder({
+        lenderIds: ["1", "2"],
+        borrowerIds: ["2", "1"],
+        amounts: [49, 49],
+        transactionIds: ["1", "2"],
+      }),
+    ],
+    [
+      "return correctly for lender and borrower from two transactions different lender and borrower",
+      {
+        ...PERSON_1,
+        balance: 49 - 30,
+        paysTo: {
+          "2": -49,
+          "3": 30,
+        },
+      },
+      PERSON_1,
+      debtsBuilder({
+        lenderIds: ["1", "3"],
+        borrowerIds: ["2", "1"],
+        amounts: [49, 30],
+        transactionIds: ["1", "2"],
+      }),
+    ],
+    [
+      "return correctly for lender and borrower from multiple transactions",
+      {
+        ...PERSON_1,
+        balance: 10 - 15 + 20 - 5 - 10 + 5,
+        paysTo: {
+          "2": -10 + 15 + 10 - 5,
+          "3": -20 + 5,
+        },
+      },
+      PERSON_1,
+      debtsBuilder({
+        lenderIds: ["1", "2", "1", "3", "2", "1"],
+        borrowerIds: ["2", "1", "3", "1", "1", "2"],
+        amounts: [10, 15, 20, 5, 10, 5],
+        transactionIds: ["1", "2", "3", "4", "5", "6"],
+      }),
+    ],
+  ])("%s", (_, expected, ...args) => {
+    expect(getBalanceOfAPerson(...args)).toEqual(expected);
   });
 });
