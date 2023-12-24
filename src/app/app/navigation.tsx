2dfaae34807ac7d6b2e79077cc550d0077571828
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2023-12-24 23:19:23 +0700

    Hide auth

--- src/app/app/navigation.tsx
+++ src/app/app/navigation.tsx
@@ -15,23 +15,23 @@ import { useTabStore } from "@hooks/useTabStore";
 export default function Navigation() {
   const activeTabId = useTabStore((state) => state.activeTabId);
   const setActiveTabId = useTabStore((state) => state.setActiveTabId);
-  const supabase = createClientComponentClient();
-  const [loading, setLoading] = useState(true);
-  const [session, setSession] = useState<Session | null>(null);
+  // const supabase = createClientComponentClient();
+  // const [loading, setLoading] = useState(true);
+  // const [session, setSession] = useState<Session | null>(null);
 
-  useEffect(() => {
-    supabase.auth.getSession().then((res) => {
-      setSession(res.data.session);
-      setLoading(false);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // useEffect(() => {
+  //   supabase.auth.getSession().then((res) => {
+  //     setSession(res.data.session);
+  //     setLoading(false);
+  //   });
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   useEffect(() => {
     useTabStore.persist.rehydrate();
   }, []);
 
-  const email = session?.user.email;
+  // const email = session?.user.email;
 
   return (
     <div className="flex flex-row justify-between gap-2 items-stretch">
@@ -51,7 +51,7 @@ export default function Navigation() {
           </button>
         ))}
       </div>
-      <button
+      {/* <button
         className="btn btn-square btn-sm shadow-none drawer-button h-10 w-10"
         onClick={() => {
           const modal = document.getElementById(
@@ -76,7 +76,7 @@ export default function Navigation() {
         ) : null}
 
         {!loading && !email ? <UserCircleIcon className="w-6 h-6" /> : null}
-      </button>
+      </button> */}
     </div>
   );
 }
