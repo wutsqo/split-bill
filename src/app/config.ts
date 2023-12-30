95643201c1228a4eece53f17cb05d97dda2c08c0
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2023-12-30 15:48:32 +0700

    Update app name in config.ts

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
-export const appName = "SplitBill";
+export const appName = "SplitEasy";
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL
   ? `https://${process.env.NEXT_PUBLIC_SITE_URL}`
   : "http://localhost:3000";
