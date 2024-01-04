2665a34ee1fcc29989fcace8828d0a7174149022
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2024-01-04 18:31:54 +0700

    Add Loading component

---
+++ src/app/app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <>
+      <div className="skeleton w-full h-10"></div>
+      <div className="skeleton w-full h-32 mt-4"></div>
+    </>
+  );
+}
