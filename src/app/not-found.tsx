836df48998f326339d5a7cf760a70e1df39e0ebd
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2024-01-04 18:23:07 +0700

    Add NotFound component

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import { ArrowUturnLeftIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="grid place-content-center h-[90vh] relative z-10">
+      <h1 className="font-taviraj text-6xl">404!</h1>
+      <h2 className="font-taviraj mt-4">This page doesn&apos;t exist.</h2>
+      <p className="prose mt-4"></p>
+      <Link href="/" className="btn btn-outline mt-16">
+        <ArrowUturnLeftIcon className="w-4 h-4 mr-2" />
+        Return Home
+      </Link>
+    </div>
+  );
+}
